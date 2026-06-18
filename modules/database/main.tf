866ba7b6df707b0ad01ab40eec3e16db372c23cd
MapNbp <matheusaraujopinh@gmail.com>
2026-06-18 13:42:17 -0300

    feat: configura alterações no terraform para deploy em prod

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,9 +1,34 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-${var.environment}-rds-sg"
+  description = "Permite conexoes PostgreSQL de qualquer lugar para testes locais"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-rds-sg"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
 resource "aws_db_instance" "postgres" {
   identifier             = "${var.project_name}-${var.environment}-db"
   allocated_storage      = var.allocated_storage
   max_allocated_storage  = var.max_allocated_storage
   engine                 = "postgres"
-  engine_version         = "15.4"
+  engine_version         = "15"
   instance_class         = var.db_instance_class
   db_name                = var.db_name
   username               = var.db_username
@@ -13,10 +38,11 @@ resource "aws_db_instance" "postgres" {
   final_snapshot_identifier = var.environment == "dev" ? null : "${var.project_name}-${var.environment}-db-final-snapshot"
 
   db_subnet_group_name   = var.db_subnet_group_name
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   storage_encrypted      = true
   deletion_protection    = var.environment == "prod" ? true : false
+  publicly_accessible    = true
 
   tags = {
     Name        = "${var.project_name}-${var.environment}-db"
